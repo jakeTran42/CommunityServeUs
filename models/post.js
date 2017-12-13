6be37805eea48808d7083c3d68dac1d769bd11cd
Donovan Adams <donovan.adams@students.makeschool.com>
2017-12-13 14:26:18 -0800

    commit of fixes for css and authen

--- models/post.js
+++ models/post.js
@@ -10,8 +10,8 @@ var PostSchema = new Schema({
   , time            : { type: String, required: true }
   , month           : { type: String, required: true }
   , day             : { type: String, required: true }
-, comments       : [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
-, author         : { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  , comments        : [{ type: Schema.Types.ObjectId, ref: 'Comment' }]
+  , author          : { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = mongoose.model('Post', PostSchema);
